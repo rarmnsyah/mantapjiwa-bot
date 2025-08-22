294709882decb25a949512e9c7d7cc0fe689140c
Risky Armansyah <rarmnsyah787@gmail.com>
2025-08-22 11:30:20 +0700

    Update webhook.js

--- netlify/functions/webhook.js
+++ netlify/functions/webhook.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 import 'dotenv/config';
 
 const supabase = createClient(
-  process.env.SUPABASE_URL,
+  'https://rmxgcafscmvdpkjalbca.supabase.co',
   process.env.SUPABASE_ANON_KEY
 );
 
